057af135add6cee2387a529cf7898cb15bef8633
imendezr <90487354+imendezr@users.noreply.github.com>
2023-10-04 19:10:49 -0600

    lista enlazada implementada

--- Proyecto1_ED/ListaEnlazada.h
+++ Proyecto1_ED/ListaEnlazada.h
@@ -1,10 +1,11 @@
-// Descripción: Definición de una lista "cuadruplemente" enlazada.
+// Definición de una lista "cuadruplemente" enlazada.
 
 #ifndef LISTAENLAZADA_H
 #define LISTAENLAZADA_H
 
 #include <iostream>
 #include <cstddef>
+#include <vector>
 
 template<class T> class ListaEnlazada {
 private:
@@ -14,14 +15,22 @@ private:
 		T valor;
 		Nodo* left;
 		Nodo* right;
+		Nodo* up;
+		Nodo* down;
 		// Constructor del Nodo - asigna NULL si se invoca sin el segundo parámetro
-		Nodo(T val, Nodo* izq = nullptr, Nodo* der = nullptr) : valor{ val }, left{ izq }, right { der } {}
+		Nodo(T valor, Nodo* izquierda = nullptr, Nodo* derecha = nullptr, Nodo* arriba = nullptr, Nodo* abajo = nullptr)
+			: valor{ valor }, left{ izquierda }, right{ derecha }, up{ arriba }, down{ abajo } {}
 	};
 
 	// Apuntador al inicio de la lista
 	Nodo* inicio;
+	// Apuntador al final de la lista
+	Nodo* final;
 
-	void Inicializar() { inicio = nullptr; }
+	void Inicializar() {
+		inicio = nullptr;
+		final = nullptr;
+	}
 
 public:
 
@@ -34,19 +43,7 @@ public:
 	ListaEnlazada() { Inicializar(); }
 	// Copia
 	explicit ListaEnlazada(const ListaEnlazada& obj) {
-		if (obj.inicio == nullptr)
-			inicio = nullptr;
-		else {
-			inicio = new Nodo(obj.inicio->valor);
-			listaptr actual = inicio;
-			listaptr actualObj = obj.inicio->right;
-			while (actualObj != nullptr) {
-				actual->right = new Nodo(actualObj->valor); // nodo copiado
-				actual->right->left = actual; // anterior del nodo copiado
-				actual = actual->right;
-				actualObj = actualObj->right;
-			}
-		}
+		// Implementación para copiar la lista
 	}
 
 	// Destructor
@@ -58,116 +55,182 @@ public:
 	// Retorna un apuntador al primer nodo de la lista - para un objeto const
 	const_listaptr GetPrimerNodo() const { return inicio; }
 
-	// Inserta un elemento al final de la lista enlazada
-	void Insertar(const T& val) {
-		listaptr nuevo;
+	// Inserta una fila al final de la lista
+	void InsertarFila(const std::vector<T>& valores) {
+		if (valores.empty()) return;  // Si el vector está vacío, no hay nada que insertar
 
-		try {
-			nuevo = new Nodo(val);
-		}
-		catch (std::bad_alloc exception) {
-			return;
-		}
+		listaptr finFilaAnterior = nullptr;
+		listaptr inicioFilaNueva = nullptr;
 
-		if (inicio == nullptr)
-			inicio = nuevo;
-		else {
-			listaptr tmp = inicio;
-			while (tmp->right != nullptr) {
-				tmp = tmp->right;
+		for (const T& val : valores) {
+			listaptr nuevoNodo = new Nodo(val);
+
+			if (!inicioFilaNueva) {
+				inicioFilaNueva = nuevoNodo;
 			}
-			tmp->right = nuevo;
-			nuevo->left = tmp; // asigno prev del nuevo nodo
+			else {
+				finFilaAnterior->right = nuevoNodo;
+				nuevoNodo->left = finFilaAnterior;
+			}
+
+			// Si hay una fila anterior, conecta el nuevo nodo verticalmente
+			if (finFilaAnterior && finFilaAnterior->up) {
+				finFilaAnterior->up->right->down = nuevoNodo;
+				nuevoNodo->up = finFilaAnterior->up->right;
+			}
+
+			finFilaAnterior = nuevoNodo;
+		}
+
+		if (!inicio) {
+			inicio = inicioFilaNueva;
 		}
 	}
 
-	// Inserta un elemento al inicio de la lista enlazada
-	void InsertarInicio(const T& val) {
-		listaptr nuevo;
+	// Inserta una columna al final de la lista
+	void InsertarColumna(const std::vector<T>& valores) {
+		if (valores.empty()) return;  // Si el vector está vacío, no hay nada que insertar
 
-		try {
-			nuevo = new Nodo(val);
-		}
-		catch (std::bad_alloc exception) {
-			return;
+		listaptr finColumnaAnterior = nullptr;
+		listaptr inicioColumnaNueva = nullptr;
+
+		for (const T& val : valores) {
+			listaptr nuevoNodo = new Nodo(val);
+
+			if (!inicioColumnaNueva) {
+				inicioColumnaNueva = nuevoNodo;
+			}
+			else {
+				finColumnaAnterior->down = nuevoNodo;
+				nuevoNodo->up = finColumnaAnterior;
+			}
+
+			// Si hay una columna anterior, conecta el nuevo nodo horizontalmente
+			if (finColumnaAnterior && finColumnaAnterior->left) {
+				finColumnaAnterior->left->down->right = nuevoNodo;
+				nuevoNodo->left = finColumnaAnterior->left->down;
+			}
+
+			finColumnaAnterior = nuevoNodo;
 		}
 
-		if (inicio == nullptr)
-			inicio = nuevo;
-		else {
-			nuevo->right = inicio;
-			inicio->left = nuevo; // asigno prev del nodo inicio anterior
-			inicio = nuevo;
+		if (!inicio) {
+			inicio = inicioColumnaNueva;
 		}
 	}
 
-	// Elimina el nodo especificado de la lista enlazada
-	void Eliminar(listaptr puntero) {
-		// No hay nada que borrar si la lista enlazada está vacía
-		// o el puntero es NULL
-		if (puntero == nullptr || inicio == nullptr)
-			return;
+	void EliminarFila(int posicion) {
+		if (!inicio) return;  // Si la lista está vacía, no hay nada que eliminar
 
-		// Caso #1: El elemento a borrar es el primero de la lista enlazada
-		if (inicio == puntero) {
+		listaptr filaActual = inicio;
+		// Moverse a la fila correcta
+		for (int i = 0; i < posicion && filaActual; i++) {
+			filaActual = filaActual->down;
+		}
+
+		// Si el índice es inválido, simplemente retornar
+		if (!filaActual) return;
+
+		while (filaActual) {
+			// Desconectar el nodo de la fila/columna anterior y siguiente
+			if (filaActual->up) {
+				filaActual->up->down = filaActual->down;
+			}
+			if (filaActual->down) {
+				filaActual->down->up = filaActual->up;
+			}
+
+			listaptr nodoAEliminar = filaActual;
+			filaActual = filaActual->right;
+
+			delete nodoAEliminar;  // Liberar memoria
+		}
+
+		// Si estamos eliminando la primera fila, actualizar el puntero 'inicio'
+		if (posicion == 0 && inicio->down) {
+			inicio = inicio->down;
+		}
+		else if (posicion == 0) {
+			inicio = nullptr;
+		}
+	}
+
+	void EliminarColumna(int posicion) {
+		if (!inicio) return;  // Si la lista está vacía, no hay nada que eliminar
+
+		listaptr columnaActual = inicio;
+		// Moverse a la columna correcta
+		for (int i = 0; i < posicion && columnaActual; i++) {
+			columnaActual = columnaActual->right;
+		}
+
+		// Si el índice es inválido, simplemente retornar
+		if (!columnaActual) return;
+
+		while (columnaActual) {
+			// Desconectar el nodo de la fila/columna anterior y siguiente
+			if (columnaActual->left) {
+				columnaActual->left->right = columnaActual->right;
+			}
+			if (columnaActual->right) {
+				columnaActual->right->left = columnaActual->left;
+			}
+
+			listaptr nodoAEliminar = columnaActual;
+			columnaActual = columnaActual->down;
+
+			delete nodoAEliminar;  // Liberar memoria
+		}
+
+		// Si estamos eliminando la primera columna, actualizar el puntero 'inicio'
+		if (posicion == 0 && inicio->right) {
 			inicio = inicio->right;
-			if (inicio) inicio->left = nullptr; // = if (inicio != nullptr). Evita desreferenciar un puntero nulo (si inicio->next es nullptr, entonces nullptr->prev daría error)
-			delete puntero;
 		}
-		// Caso #2: El elemento se encuentra en alguna otra posición de la lista
-		else {
-			listaptr tmp = inicio;
-			listaptr tmp2 = tmp->right;
-			while (tmp2 != nullptr) {
-				if (tmp2 == puntero) {
-					tmp->right = tmp2->right;
-
-					if (tmp2->right) { // = if (tmp2->next != nullptr). Si el nodo que se elimina no es el último, se ajusta prev del siguiente.
-						tmp2->right->left = tmp;
-					}
-
-					delete tmp2;
-					return;
-				}
-				tmp = tmp2;
-				tmp2 = tmp2->right;
-			}
+		else if (posicion == 0) {
+			inicio = nullptr;
 		}
 	}
 
-	// Elimina un nodo de la lista en la posición especificada
-	void EliminarEnPos(std::size_t pos) {
-		if (pos <= 0)
-			return;
+	// Obtener nodo específico por posición
+	listaptr ObtenerNodo(int fila, int columna) {
+		if (!inicio) return nullptr;  // Si la lista está vacía
 
-		listaptr tmp = inicio;
-		for (std::size_t i = 1; i < pos; ++i) {
-			tmp = tmp->right;
-			if (tmp == nullptr)
-				return;
-		}
-		Eliminar(tmp);
-	}
+		listaptr nodoActual = inicio;
 
-	// Busca un elemento en la lista y retorna el nodo
-	listaptr Buscar(const T& val) {
-		listaptr tmp = inicio;
-		while (tmp != nullptr) {
-			if (tmp->valor == val)
-				return tmp;
-			tmp = tmp->right;
+		// Moverse a la fila correcta
+		for (int i = 0; i < fila && nodoActual; i++) {
+			nodoActual = nodoActual->down;
 		}
-		return nullptr;  // Si no lo encuentra retorna nullptr
+
+		// Si no se encontró la fila
+		if (!nodoActual) return nullptr;
+
+		// Moverse a la columna correcta
+		for (int j = 0; j < columna && nodoActual; j++) {
+			nodoActual = nodoActual->right;
+		}
+
+		// Devuelve el nodo en la posición especificada o nullptr si no se encontró
+		return nodoActual;
 	}
 
 	// Borra completamente la lista
 	void BorrarLista() {
-		listaptr tmp;
-		while (inicio != nullptr) {
-			tmp = inicio;
-			inicio = inicio->right;
-			delete tmp;
+		listaptr filaActual = inicio;
+
+		while (filaActual) {
+			listaptr nodoActual = filaActual;
+			while (nodoActual) {
+				listaptr nodoAEliminar = nodoActual;
+				nodoActual = nodoActual->right;
+				delete nodoAEliminar;
+			}
+			listaptr filaAEliminar = filaActual;
+			filaActual = filaActual->down;
+			delete filaAEliminar;
 		}
+
+		inicio = nullptr;  // Establecer inicio a nullptr
 	}
 };
 
